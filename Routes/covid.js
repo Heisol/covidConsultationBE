905466d3eb4d67b2781375f95df84625a49675eb
Heisol <alidejando@gmail.com>
2021-11-04 18:50:15 +0800

    log req

--- Routes/covid.js
+++ Routes/covid.js
@@ -10,6 +10,7 @@ router.use(cors())
 
 router.post("/", cors(), upload.none(),async (req, res) => {
   const newReqBody = {symptoms: JSON.parse(req.body.symptoms), symptomsVal: JSON.parse(req.body.symptomsVal)}
+  console.log(newReqBody)
   const schema = Joi.object({
     symptoms: Joi.array().items(Joi.string().required()).required(),
     symptomsVal: Joi.array().items(Joi.bool().required()).required(),
